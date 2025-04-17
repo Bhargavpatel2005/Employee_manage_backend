a3d270baefeb48e8985ff841f3e1cf45c4aa3306
Bhargav Patoliya <patoliyabhargav3@gmail.com>
2025-04-17 12:23:05 +0530

    env create and database cheang

--- HR_py/settings.py
+++ HR_py/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'HR_py.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'HR_manage',
+        'USER': 'bnbrun',
+        'PASSWORD': 'bnbrun',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
